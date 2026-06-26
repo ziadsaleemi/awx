8380b6027141e64646fb6ecadd16ac5700b2c71b
Ziad Saleemi <ziad.saleemii@gmail.com>
2026-06-26 00:28:12 -0400

    Prepare AWX 25.1.3 hotfix release
    Signed-off-by: Ziad Saleemi <ziad.saleemii@gmail.com>

--- awx/ui/src/frontend/common/AboutModal.tsx
+++ awx/ui/src/frontend/common/AboutModal.tsx
@@ -8,7 +8,7 @@ export interface AnsibleAboutModalProps {
   onClose?: () => void;
 }
 
-export const ABOUT_MODAL_VERSION = '25.1.2';
+export const ABOUT_MODAL_VERSION = '25.1.3';
 
 function AnsibleAboutModal(props: AnsibleAboutModalProps) {
   const [_dialog, setDialog] = usePageDialog();
