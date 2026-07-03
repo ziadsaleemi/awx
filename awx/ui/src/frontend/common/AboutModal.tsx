fda8ebf0781f3a76e438fac0e77b11d0d6ff09df
Ziad Saleemi <ziad.saleemii@gmail.com>
2026-07-03 13:37:57 -0400

    Prepare AWX 25.1.6 release
    Signed-off-by: Ziad Saleemi <ziad.saleemii@gmail.com>

--- awx/ui/src/frontend/common/AboutModal.tsx
+++ awx/ui/src/frontend/common/AboutModal.tsx
@@ -8,7 +8,7 @@ export interface AnsibleAboutModalProps {
   onClose?: () => void;
 }
 
-export const ABOUT_MODAL_VERSION = '25.1.4';
+export const ABOUT_MODAL_VERSION = '25.1.6';
 
 function AnsibleAboutModal(props: AnsibleAboutModalProps) {
   const [_dialog, setDialog] = usePageDialog();
